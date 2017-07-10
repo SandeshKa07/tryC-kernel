33a272838883124dd0531f047681ea7741dd57cf
Sandesh K A <kas.sandesh@gmail.com>
2017-07-10 12:20:05 +0530

    [misc char] fixed coding sytle errors
    linux kernel source checkpatch.pl is used to find the coding style errors and warnings

--- misc_char_driver.c
+++ misc_char_driver.c
@@ -1,18 +1,19 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/miscdevice.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/printk.h>
 
 #define myassignID "a89cef62eae7"
 #define idLen 13
 
-static ssize_t my_misc_read(struct file *file, char __user *buffer, size_t count, loff_t *ppos)
+static ssize_t my_misc_read(struct file *file, char __user *buffer,
+			    size_t count, loff_t *ppos)
 {
 	if (*ppos != 0)
 		return 0;
-	
+
 	if ((count < idLen) || copy_to_user(buffer, myassignID, idLen))
 		return -EFAULT;
 
@@ -20,23 +21,24 @@ static ssize_t my_misc_read(struct file *file, char __user *buffer, size_t count
 	return count;
 }
 
-static ssize_t my_misc_write(struct file *file, const char __user *buffer, size_t count, loff_t *ppos)
+static ssize_t my_misc_write(struct file *file, const char __user *buffer,
+			     size_t count, loff_t *ppos)
 {
 	char wbuffer[idLen];
-	
-	if(count != idLen)
+
+	if (count != idLen)
 		return -EINVAL;
-	   
-	if(copy_from_user(wbuffer, buffer, count))
+
+	if (copy_from_user(wbuffer, buffer, count))
 		return -EFAULT;
 
-	if(strncmp(myassignID, wbuffer, (idLen - 1)))
+	if (strncmp(myassignID, wbuffer, (idLen - 1)))
 		return -EINVAL;
 
 	return count;
 }
 
-const static struct file_operations my_misc_fops = {
+static const struct file_operations my_misc_fops = {
 	.owner = THIS_MODULE,
 	.read = my_misc_read,
 	.write = my_misc_write,
@@ -51,16 +53,16 @@ static struct miscdevice my_misc_device = {
 static int __init my_misc_init(void)
 {
 	int error;
+
 	pr_info("\nHello World\n");
 	error = misc_register(&my_misc_device);
-	if(error)
-	{
+	if (error) {
 		pr_err("\n Error Registering the misc device\n");
 		return error;
 	}
 	return 0;
 }
-       
+
 static void __exit my_misc_exit(void)
 {
 	misc_deregister(&my_misc_device);
