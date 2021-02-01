8bd0b0c624e0c0488797791cf4f673986ef4310e
Sandesh Kenjana Ashok <kas.sandesh@gmail.com>
2021-02-01 22:55:58 +0100

    [Function Pointer] Function pointer declaration and invoking
    Signed-off-by: Sandesh Kenjana Ashok <kas.sandesh@gmail.com>

---
+++ basics/function_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+static void my_function(int a){
+	printf("My Function called with argument = %d\n",a);
+}
+
+int main(){
+	void (*my_func_ptr)(int) = &my_function; //assigning function to a function pointer
+
+	/* Calling the function with an argument*/
+	(*my_func_ptr)(10);
+
+	return 0;
+}
